5b3774bd39e8b0be28d511a1990df4fa47a647db
leonel meirinho <leonel meirinho>
2017-06-13 15:32:12 +0200

    test heroku2

--- server/routes/products/handlers/getAll.js
+++ server/routes/products/handlers/getAll.js
@@ -3,8 +3,6 @@ const Product = require(__base + 'models/product')
 function getAll(req, res) {
 
   Product.find({})
-        .populate('shop')
-        .exec(products => products)
         .then(products => res.json(products))
 }
 
