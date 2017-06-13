63ed74413a10725ddb5b6e99cc53fb7b91c78ff1
leonel meirinho <leonel meirinho>
2017-06-13 15:52:28 +0200

    test heroku2

--- server/routes/products/handlers/getAll.js
+++ server/routes/products/handlers/getAll.js
@@ -3,9 +3,9 @@ const Product = require(__base + 'models/product')
 function getAll(req, res) {
 
   Product.find({})
-        .populate('shop')
+        .populate('shops')
         .exec(products => res.json(products))
-        .then(()  => res.status(200).json({ msg: 'product show...'}))
+        //.then(()  => res.status(200).json({ msg: 'product show...'}))
 }
 
 module.exports = getAll
